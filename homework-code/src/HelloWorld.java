0ad835c5e248ab51e0569a959bcd313ea3e9d3ec
hixiaodong123 <420350696@qq.com>
2021-07-04 20:59:39 +0800

    长风老师提交

---
+++ homework-code/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @description: test
+ * @author: wuguidong@cskaoyan.onaliyun.com
+ **/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
