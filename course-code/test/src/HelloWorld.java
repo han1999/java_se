14ab122ac5ef6b9b5a2ec28e64d21b2134f72b50
hixiaodong123 <420350696@qq.com>
2021-07-05 08:13:01 +0800

    长风老师的提交

--- course-code/test/src/HelloWorld.java
+++ course-code/test/src/HelloWorld.java
@@ -1,8 +1,28 @@
 /**
- * @description: test
+ * @description: 我的第一行代码: 你好世界!
  * @author: wuguidong@cskaoyan.onaliyun.com
  **/
 
+/**
+ * 本class目的: 怎么运行从Git上下载下来的代码?
+ *      1,根据class右上角的提示setup SDK,直接选择8或者指定你的jdk8安装目录
+ *      2,然后按住ctrl + alt + shift + s 打开project structure
+ *          或者左上角 file --> project structure
+ *      3,查看其中的project选项页
+ *          然后将project language level 设置为8
+ *          project compiler output目录要设置出来:
+ *          建议设置为
+ *          当前目录/course-code/out
+ *      5,设置文件夹权限.查看其中Modules选项页:
+ *          将src目录设置为蓝色的Sources权限
+ *          其余不要乱动
+ *
+ *      6,以上,就可以运行代码了
+ *
+ *  补做:
+ *
+ *
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world!");
