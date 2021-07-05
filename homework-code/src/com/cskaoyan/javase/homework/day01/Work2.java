41d60e0feae2359420e3ba52c9c53d0f89ce2d20
hixiaodong123 <420350696@qq.com>
2021-07-05 22:08:28 +0800

    长风老师的提交

--- homework-code/src/com/cskaoyan/javase/homework/day01/Work2.java
+++ homework-code/src/com/cskaoyan/javase/homework/day01/Work2.java
@@ -27,28 +27,28 @@ public class Work2 {
             taxable = salary - 2000;
             double taxRate = 0;//税率
             double deduction = 0;//速算扣除数
-            if (taxable <= 500 ) {
+            if (taxable <= 500) {
                 taxRate = 0.05;
                 deduction = 0;
-            } else if (taxable > 500 && (taxable <= 2000)) {
+            } else if (taxable <= 2000) {
                 taxRate = 0.1;
                 deduction = 25;
-            } else if (taxable > 2000 && (taxable <= 5000)) {
+            } else if (taxable <= 5000) {
                 taxRate = 0.15;
                 deduction = 125;
-            } else if (taxable > 5000 && (taxable <= 20000)) {
+            } else if (taxable <= 20000) {
                 taxRate = 0.2;
                 deduction = 375;
-            } else if (taxable > 20000 && (taxable <= 40000)) {
+            } else if (taxable <= 40000) {
                 taxRate = 0.25;
                 deduction = 1375;
-            } else if (taxable > 40000 && (taxable <= 60000)) {
+            } else if (taxable <= 60000) {
                 taxRate = 0.3;
                 deduction = 3375;
-            } else if (taxable > 60000 && (taxable <= 80000)) {
+            } else if (taxable <= 80000) {
                 taxRate = 0.35;
                 deduction = 6375;
-            } else if (taxable > 80000 && (taxable <= 100000)) {
+            } else if (taxable <= 100000) {
                 taxRate = 0.4;
                 deduction = 10375;
             } else if (taxable > 100000) {
@@ -62,7 +62,6 @@ public class Work2 {
 
             System.out.println("缴纳的个人所得税为：" + tax);
             System.out.println("实际到手工资为：" + salaryLeft);
-
 
 
             //------------
